28dce71ae8fea257558bce1d3c20e66cf555eae3
Gunju Ra <gunju@Gunjuui-MacBookPro.local>
2020-07-03 01:44:38 +0900

    complete

---
+++ src/main/java/com/test/gunju/book/config/JpaConfig.java
@@ -0,0 +1,11 @@
+package com.test.gunju.book.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration("JpaConfig")
+@EnableJpaAuditing
+public class JpaConfig {
+
+}
--- src/main/java/com/test/gunju/book/config/auth/dto/SessionUser.java
+++ src/main/java/com/test/gunju/book/config/auth/dto/SessionUser.java
@@ -3,16 +3,17 @@ package com.test.gunju.book.config.auth.dto;
 import com.test.gunju.book.domain.user.User;
 import lombok.Getter;
 
+import java.io.Serializable;
+
 @Getter
-public class SessionUser {
+public class SessionUser implements Serializable {
     private String name;
     private String email;
     private String picture;
 
-    public SessionUser(User user){
-        this.name=user.getName();
-        this.email=user.getEmail();
-        this.picture=user.getPicture();
-        this.picture=user.getPicture();
+    public SessionUser(User user) {
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.picture = user.getPicture();
     }
 }
